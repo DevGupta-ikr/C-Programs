0e62e655bc8565ca74fdcb55f69de21720d3472a
DevGupta <77541795+DevGupta-ikr@users.noreply.github.com>
2021-09-19 19:47:02 +0530

    Add files via upload

---
+++ Basic_Programs/Sum_of_integers.c
@@ -0,0 +1,14 @@
+/* Sum of two integers  */
+
+#include<stdio.h>
+
+int main()
+{
+    int num1, num2, sum;
+    printf("Enter the two integer values to be added ");
+    scanf("%d %d", &num1, &num2);
+    sum = num1 + num2;
+    printf("Addition of %d and %d = %d\n", num1, num2, sum);
+
+    return 0;
+}
